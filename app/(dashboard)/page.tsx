f1551a33885ee5838f9a17f3134754334b42d1a5
moreorover <moreorover@null.computer>
2024-05-15 23:01:33 +0100

    trpc

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,17 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
+
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 
 export default function Home() {
+  const accountsQuery = useGetAccounts();
   return (
     <div>
       <p>Dashboard page</p>
-      <UserButton afterSignOutUrl="/" />
+      <div>
+        {accountsQuery.data?.map((account) => (
+          <div key={account.id}>{account.name}</div>
+        ))}
+      </div>
     </div>
   );
 }
