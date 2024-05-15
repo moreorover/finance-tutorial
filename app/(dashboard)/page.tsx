e652a33d307c0caf5dc56a59397295824987a377
moreorover <moreorover@null.computer>
2024-05-15 19:39:42 +0100

    header logo

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,10 @@
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <UserButton afterSignOutUrl="/" />;
+  return (
+    <div>
+      <p>Dashboard page</p>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
